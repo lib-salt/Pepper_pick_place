862c52f5495e2dd1546cef8fd873955a95cfd313
lib-salt <130981325+lib-salt@users.noreply.github.com>
2025-02-07 14:22:47 +0000

    object_detector.py

--- object_detector.py
+++ object_detector.py
@@ -2,7 +2,6 @@ import os
 import cv2
 import numpy as np
 import mediapipe as mp
-from naoqi import ALProxy 
 
 class ObjectDetector():
     def __init__(self):
@@ -74,37 +73,4 @@ class ObjectDetector():
         cap.release()
         cv2.destroyAllWindows()
 
-    def get_video_feed(ip_address):
-        port = 9559
-        global frame_buffer
-        try:
-            # Create a proxy to ALVideoDevice
-            video_service = ALProxy("ALVideoDevice", ip_address, port)
-
-            resolution = 2  
-            color_space = 11  
-            fps = 30
-            name = "pepper_camera"
-            cam_name = video_service.subscribeCamera(name, 0, resolution, color_space, fps)
-
-            while True:
-                # Get image remotely
-                frame = video_service.getImageRemote(cam_name)
-                if frame is not None:
-                    image_width = frame[0]
-                    image_height = frame[1]
-                    array = frame[6]
-                    image_np = cv2.cvtColor(np.fromstring(array, dtype=np.uint8).reshape((image_height, image_width, 3)), cv2.COLOR_BGR2RGB)
-
-                    detect_objects()
-
-                else:
-                    print("Failed to get frame")
-                    
-        except Exception as e:
-            print(f"Error: {e}")
-        finally:
-            # Unsubscribe and cleanup
-            if 'cam_name' in locals():
-                video_service.unsubscribe(cam_name)
 
