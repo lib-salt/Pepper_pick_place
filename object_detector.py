654a313f1728f470e809d22774e9f42010717ced
lib-salt <130981325+lib-salt@users.noreply.github.com>
2025-02-14 13:20:35 +0000

    Update object_detector.py

--- object_detector.py
+++ object_detector.py
@@ -3,12 +3,14 @@ import numpy as np
 import socket
 import struct
 import pickle
+import sys
+sys.path.append(r"D:\pynaoqi-python2.7-2.5.5.5-win32-vs2013\lib")
 from naoqi import ALProxy
 
-server_ip = '192.168.x.x'  
+server_ip = '127.0.0.1'  
 server_port = 12345 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((server_ip, server_port))
+# sock.connect((server_ip, server_port))
 
 def get_video_feed(ip_address):
     port = 9559
@@ -31,17 +33,30 @@ def get_video_feed(ip_address):
                 array = frame[6]
                 image_np = cv2.cvtColor(np.fromstring(array, dtype=np.uint8).reshape((image_height, image_width, 3)), cv2.COLOR_BGR2RGB)
 
-                # Send the frame to the laptop
-                frame_data = pickle.dumps(image_np)
-                sock.sendall(struct.pack("L", len(frame_data)) + frame_data)
+                # # Send the frame to the laptop
+                # frame_data = pickle.dumps(image_np)
+                # sock.sendall(struct.pack("L", len(frame_data)) + frame_data)
+            
+                try:
+                    # Send the frame to the laptop
+                    frame_data = pickle.dumps(image_np)
+                    sock.sendall(struct.pack("L", len(frame_data)) + frame_data)
+                except socket.error as e:
+                    print("Error sending frame: %s" % e)
+                    break
+
+            if cv2.waitKey(1) & 0xFF == 27:  # Press ESC to exit
+                print("Closing window")
+                break
 
     except Exception as e:
-        print(f"Error: {e}")
+        print("Error: %s" % e)
     finally:
         if 'cam_name' in locals():
             video_service.unsubscribe(cam_name)
         sock.close()
 
 # Start sending frames
-get_video_feed('192.168.x.x')  
+sock.connect((server_ip, server_port))
+get_video_feed('192.168.245.57')  
 
