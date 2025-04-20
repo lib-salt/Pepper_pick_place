5fa7d6db0e8ca8597cebcedaae83ce946cdeff9c
lib-salt <130981325+lib-salt@users.noreply.github.com>
2025-04-20 13:23:50 +0100

    changes

--- pepper_pipeline/behaviour_controller.py
+++ pepper_pipeline/behaviour_controller.py
@@ -1,5 +1,4 @@
 import time
-import math
 from utils import logger
 from robot_map import PepperRobotMap
 
@@ -12,6 +11,8 @@ class BehaviourController:
         self.network_listener = network_listener
         self.running = False
         self.detected_objects = []
+        self.target_positions = []
+        self.actual_positions = []
 
     def start(self):
         self.running = True
@@ -30,6 +31,14 @@ class BehaviourController:
 
     def run_object_tracking(self):
         old_category = None
+
+        # Start position for mapping
+        initial_pos = {
+            'x': 0,
+            'y': 0,
+            'theta': 0,
+        }
+        self.actual_positions.append(initial_pos)
         
         while self.running:
             try:
@@ -45,6 +54,10 @@ class BehaviourController:
 
                 if position is None:
                     logger.warning("Could not get position data, skipping")
+                    continue
+
+                if depth is None:
+                    logger.warning("Could not get depth data, skipping")
                     continue
 
                 logger.info("Depth: {}".format(depth))
@@ -72,19 +85,48 @@ class BehaviourController:
                 self.detected_objects.append(obj)
 
                 # Update map
-                self._update_map(self.detected_objects)
+                self._update_map(self.detected_objects, self.actual_positions)
 
                 # Move towards object
-                x = position["x"] - 0.18 # Add distance buffer to lift arm
-                y = position["y"] + 0.15 # 20 (moveTo method) # Additional 10cm to line up right arm
+                x = position["x"] - 0.10 # Add distance buffer to lift arm
+                y = position["y"] + 0.15 # Additional movement to line up right arm
                 theta = position["theta"]
+
+                # Calculate target position relative to the current position
+                start_pos = self.motion_controller.get_robot_position()
+                target_position = {
+                    'x': x,
+                    'y': y,
+                    'theta': theta,
+                }
+                self.target_positions.append(target_position)
+
+                self._update_map(self.detected_objects, self.actual_positions, self.target_positions)
 
                 # Move to object
                 success = self.motion_controller.move_to_position(x, y, theta)
 
                 if success:
                     # Wait for movement to comeplete
-                    time.sleep(5)
+                    time.sleep(2)
+
+                    # Get the new position of the robot
+                    new_pos = self.motion_controller.get_robot_position()
+                    # Position of robot for 2D map 
+                    map_pos = [new - old for new, old in zip(new_pos, start_pos)]
+
+                    actual_position = {
+                    'x': map_pos[0],
+                    'y': map_pos[1],
+                    'theta': map_pos[2],
+                    }
+                    self.actual_positions.append(actual_position)
+
+                    # Update map with robot positions
+                    self._update_map(self.detected_objects, self.actual_positions, self.target_positions)
+
+                    # Announce reaching object position
+                    self.speech_manager.announce_reaching_object(category)
 
                     # Tilt head to keep seeing object
                     self.motion_controller.tilt_head(0.3, 0.2)
@@ -101,8 +143,8 @@ class BehaviourController:
             except Exception as e:
                 logger.error("Error in object tracking loop: {}".format(e))
 
-    def _update_map(self, detected_objects):
+    def _update_map(self, detected_objects, actual_positions=None, target_positions=None):
         if detected_objects:
             robot_map = PepperRobotMap()
-            robot_map.update_map(detected_objects)
+            robot_map.update_map(detected_objects, actual_positions, target_positions)
             map_filepath = robot_map.save()
