cea0717b9a00de4548d14e8dc2541336767ecc31
lib-salt <130981325+lib-salt@users.noreply.github.com>
2025-04-20 13:36:16 +0100

    changes

--- pepper_pipeline/speech_manager.py
+++ pepper_pipeline/speech_manager.py
@@ -1,74 +1,47 @@
-import socket
-import logging
-import time
 from utils import logger
 
-class NetworkListener:
-    def __init__(self):
-        # Initialise network listener
-        self.location_sock = None
-        self.running = False
+class SpeechManager:
+    def __init__(self, speech_proxy=None):
+        self.speech_proxy = speech_proxy
 
-    def initialise(self, server_ip, port):
-        # Initialise object tracker socket
-        try:
-            self.location_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.location_sock.bind((server_ip, port))
-            self.running = True
-            logger.info("Network listener initialised on {}.{}".format(server_ip, port))
-            return True
+        # Initialise speech settings
+        if self.speech_proxy:
+            self.speech_proxy.setLanguage("English")
+            logger.info("Speech manager initialised")
+
+    def say(self, text):
+        # Make Pepper speak
+        if self.speech_proxy:
+            try:
+                self.speech_proxy.say(text)
+                logger.debug("Pepper said: {}".format(text))
+
+            except Exception as e:
+                logger.error("Error making Pepper talk: {}".format(e))
+
+    def announce_object(self, category):
+        # Announce object detection
+        text = "I have found a {}".format(category)
+        self.say(text)
+
+    def announce_reaching_object(self, category):
+        # Announce when Pepper has reached the object
+        text = "I have reached the {}".format(category)
+        self.say(text)
+
+    def announce_searching(self):
+        # Announce that Pepper is searching
+        text = "I am looking for objects"
+        self.say(text)
+
+    def announce_error(self, error_type="general"):
+        # Announce errors
+        error_messages = {
+            "position": "I cannot determine the position of the object",
+            "movement": "I cannot reach the object",
+            "detection": "I cannot detect any objects",
+            "general": "I encountered a problem"
+        }
         
-        except Exception as e:
-            logger.error("Error initialising network listener: {}".format(e))
-
-    def cleanup(self):
-        # Cleanup network resources
-        try:
-            self.running = False
-            if self.location_sock:
-                self.location_sock.close()
-            logger.info("Network listener cleaned up")
-
-        except Exception as e:
-            logger.error("Error during network listener cleanup: {}".format(e))
-
-    def get_object_coords(self):
-        try:
-            if not self.running or not self.location_sock:
-                logger.error("Network listener not initialised")
-                return None, None, None
-                       
-            # Receive coordinate data
-            data, _ = self.location_sock.recvfrom(1024)
-
-            if not data:
-                logger.warning("No data received from object detector")
-                return None, None, None
-            
-            # Decode data from socket
-            coords = data.decode().split(',')
-            if len(coords) < 3:
-                logger.warning("Invalid data format received: {}".format(data))
-                return None, None, None
-            
-            x_cen = float(coords[0])
-            y_cen = float(coords[1])
-            category = str(coords[2])
-            timestamp = float(coords[3])
-
-            current_time = time.time()
-
-            time_dif = current_time - timestamp
-
-            # Check if co-ordinates are older than 2 seconds
-            if time_dif > 2.0:
-                logger.warning("No new object detected")
-                return None, None, None
-
-            logger.info("Detected {0} at coordinates ({1}, {2})".format(category, x_cen, y_cen))
-
-            return x_cen, y_cen, category
-        
-        except Exception as e:
-            logger.error("Failed to get coordinates: {}".format(e))
-            return None, None, None
+        text = error_messages.get(error_type, error_messages["general"])
+        self.say(text)
