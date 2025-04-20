aad647eb1258f9d6445ac99fb87a5c681f455405
lib-salt <130981325+lib-salt@users.noreply.github.com>
2025-04-20 12:40:23 +0100

    changes

--- object_recognition/mediapipe_recognition.py
+++ object_recognition/mediapipe_recognition.py
@@ -32,15 +32,6 @@ def process_frame(frame):
             print("Error: Received empty frame.")
             return None
         
-        # Border to show depth image size
-        x1 = (320 - 320) // 2
-        y1 = (240 - 180) // 2
-        x2 = x1 + 320
-        y2 = y1 + 180
-
-        # Draw the border of depth image
-        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)  
-
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
         result = detector.detect(mp_image)
 
@@ -54,8 +45,11 @@ def process_frame(frame):
                 bbox = detection.bounding_box
                 x, y, w, h = int(bbox.origin_x), int(bbox.origin_y), int(bbox.width), int(bbox.height)
 
+                category_info = detection.categories[0]
+                score_percent = int(category_info.score * 100)
+                label = f"{category} {score_percent}%"
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.putText(frame, category, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
                 # Calculate object center
                 x_cen = x + w // 2
